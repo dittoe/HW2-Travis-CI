d84249ff3d8933fb939798cb0625e53027a3483a
Nicholas Dittoe <nicholas.dittoe@gmail.com>
2018-03-01 16:55:56 -0500

    Please work

---
+++ src/Calc.java
@@ -0,0 +1,25 @@
+// Introduction to Software Testing
+// Authors: Paul Ammann & Jeff Offutt
+// Chapter 3; page ??
+// See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
+//Heres the comment to check
+
+public class Calc
+{
+   static public int add (int a, int b)
+   {
+      return a + b;
+   }
+   
+   static public int subtract(int a, int b)	{
+	   return a - b;
+   }
+   
+   static public int multiply (int a, int b)	{
+	   return a * b;
+   }
+   
+   static public int divide (int a, int b)	{
+	   return a / b;
+   }
+}
